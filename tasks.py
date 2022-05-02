bd3cd9bff7dca2d39a5a0f1f44c61b7634e65730
michal <michal.wilk0@yahoo.com>
2022-05-03 00:43:44 +0200

    added invoke to the requirements

--- tasks.py
+++ tasks.py
@@ -4,5 +4,16 @@ from invoke import task
 
 
 @task
-def test():
+def test_invoke(c):
     print("hello world")
+
+@task
+def test(c, unit_test=False, system_tests=False):
+    if unit_test:
+        c.run("echo 'Running unittests'")
+    
+    if system_tests:
+        c.run("echo 'Running system tests'")
+    
+    if not (unit_test or system_tests):
+        c.run("echo {}".format("You have not specified what tests do you want to run"))
