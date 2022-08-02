02cbca53a8f6f0dc5ec0fc3a422adb90c8b2ed4a
michal <michal.wilk0@yahoo.com>
2022-08-02 21:31:06 +0200

    create lexer functions

--- src/wolth/db/query.clj
+++ src/wolth/db/query.clj
@@ -12,11 +12,6 @@
 (comment
   (create-pairs (range 10))
   (create-pairs (range 11)))
-; Available operations:
-; - sort "<<" ">>"
-; - filter - znaki: "==" "<>" ">" ">=" "LEN"
-; - * (all)
-; - detail - zwykły znak "="
 
 ;; TODO: CHANGE THIS!!!
 (defn can-join? [pair] true)
@@ -35,9 +30,61 @@
 
 (comment
   (check-if-joining-valid
-    '(("person" "id=1") ("comment" "*") ("movie" "min-age>12"))))
+   '(("person" "id=1") ("comment" "*") ("movie" "min-age>12"))))
 
 (def objects-url-spec (atom {}))
+
+; Available operations:
+; - sort "<<" ">>"
+; - filter - znaki: "==" "<>" ">" ">=" "LEN". 
+;    Połączone za pomocą 
+; - * (all)
+; - detail - zwykły znak "="
+
+;; (def)
+
+(def detail-reg #"(?i)[a-z]+[0-9]*=[a-z0-9]+")
+(def all-reg #"\*")
+(def sort-reg #"((<<)|(>>)[a-zA-Z]+[0-9]*)+")
+;; (def list-reg #"()")
+
+(defn detail-lexer [val] 
+  ( re-matches detail-reg val))
+
+(comment
+  (detail-lexer "id=123")
+  (detail-lexer "123id=123")
+  (detail-lexer "id=123name=2")
+  )
+
+
+(defn all-lexer [val] 
+  ( re-matches all-reg val))
+
+(comment
+  (all-lexer "*")
+  (all-lexer "qwerty")
+  )
+
+(defn sort-lexer [val]
+  (re-matches sort-reg val))
+
+(defn filter-lexer [val]
+  (re-matches sort-reg val))
+
+(defn list-lexer [val] (assert false "not implemented"))
+
+(comment
+  list-lexer
+  )
+
+(defn parse-query-selector [selector]
+  )
+
+(comment
+  (parse-query-selector "id=123")
+  (parse-query-selector "<<sort<<name?(name==Adam#(age<10&(name<>Patrick#age<=100)))")
+  )
 
 (defn build-subquery [table-name selector] "DSKNDKJSNKJDN")
 
