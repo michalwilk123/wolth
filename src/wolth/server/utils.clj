a3597cfd356674c2862271a32e5e850e60ed8380
michalwilk <michal.wilk0@yahoo.com>
2022-10-05 13:47:24 +0200

    remove example data

--- src/wolth/server/utils.clj
+++ src/wolth/server/utils.clj
@@ -1,5 +1,7 @@
 (ns wolth.server.utils
   (:require [clojure.string :as str]
+            [wolth.server.config :refer [app-data-container]]
+            [wolth.server.exceptions :refer [throw-wolth-exception]]
             [wolth.utils.common :as common]))
 
 (def _test-app-data
@@ -11,15 +13,25 @@
           {:constraints [:not-null], :name "role", :type :str128}
           {:constraints [:unique], :name "email", :type :str128}],
        :options [:uuid-identifier]}],
+   :functions [{:name "getDate"}],
    :serializers [{:name "public",
-                  :model "User",
-                  :read {:fields ["author" "content" "id"], :attached []},
-                  :update {:fields ["username" "email"]},
-                  :create {:fields ["username" "email" "password"],
-                           :attached [["role" "regular"]]},
-                  :delete true}]})
+                  :allowed-roles ["admin"],
+                  :operations
+                    [{:model "User",
+                      :read {:fields ["author" "content" "id"], :attached []},
+                      :update {:fields ["username" "email"]},
+                      :create {:fields ["username" "email" "password"],
+                               :attached [["role" "regular"]]},
+                      :delete true}]}]})
 
-(defn get-associated-app-data! [app-name] _test-app-data)
+(defn get-associated-app-data!
+  [app-name]
+  (if-let [app-data (get @app-data-container app-name)]
+    app-data
+    (throw-wolth-exception :404 (format "Application %s not found" app-name))))
+
+(comment
+  (get-associated-app-data! "app"))
 
 (defn get-associated-objects
   [app-data table-names]
@@ -28,12 +40,6 @@
 (comment
   (get-associated-objects _test-app-data (list "User")))
 
-(defn get-serializer-data
-  [app-data serializer-name]
-  (first (filter #(= (% :name) serializer-name) (app-data :serializers))))
-
-(comment
-  (get-serializer-data _test-app-data "public"))
 
 (defn uri->parsed-info
   [uri method]
@@ -53,3 +59,13 @@
 (comment
   (uri->parsed-info "/app/Person/public" :post)
   (uri->parsed-info "/app/Person/firstquery/NextTable/*/public" :get))
+
+(defn uri->app-name [uri] (second (str/split uri #"/")))
+
+(comment
+  (uri->app-name "/app/Person/public"))
+
+(defn utility-context [ctx] (merge ctx {:app-name (uri->app-name (ctx :uri))}))
+
+(def utility-interceptor
+  {:name ::UTILITY-INTERCEPTOR, :enter utility-context})
