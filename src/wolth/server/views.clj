3124614b4599e49c5352313a5dc7b8935abe0858
michalwilk <michal.wilk0@yahoo.com>
2022-10-09 23:58:39 +0200

    update view interceptor

--- src/wolth/server/views.clj
+++ src/wolth/server/views.clj
@@ -1,16 +1,34 @@
 (ns wolth.server.views
-  (:require [wolth.server.exceptions :refer [def-interceptor-fn]]))
+  (:require [wolth.server.exceptions :refer [def-interceptor-fn]]
+            [wolth.utils.common :refer [sql-map->map]]))
 
 (def-interceptor-fn
-  view-interceptor-fn
+  model-view-interceptor-fn
   [ctx]
-  (assoc ctx
-    :response {:status 200,
-               :body (str "Success!!"
-                          (select-keys ctx
-                                       [:response :exception-occured :sql-query
-                                        :result]))}))
+  (if-let [result (get ctx :result)]
+    (assoc ctx
+      :response (case (get-in ctx [:request :request-method])
+                  :get {:status 200,
+                        :body (if (coll? result)
+                                (map sql-map->map result)
+                                (sql-map->map result))}
+                  :post {:status 201, :body "Created data"}
+                  :patch {:status 200, :body "Updated data"}
+                  :delete {:status 200, :body "Deleted data"}))
+    (assoc ctx
+      :response {:status 200,
+                 :body "No result from resolver, but request was successful"})))
 
-(def view-interceptor
-  {:name ::WOLTH-VIEW-INTERCEPTOR, :enter view-interceptor-fn})
+(def-interceptor-fn function-view-interceptor-fn
+                    [ctx]
+                    (if (get ctx :result)
+                      (assoc ctx
+                        :response {:status 200, :body (get ctx :result)})
+                      (assoc ctx :response {:status 204})))
+
+(def model-view-interceptor
+  {:name ::WOLTH-VIEW-INTERCEPTOR, :enter model-view-interceptor-fn})
+
+(def function-view-interceptor
+  {:name ::WOLTH-VIEW-INTERCEPTOR, :enter function-view-interceptor-fn})
 
