24691e18c3651ffa7fb495230184eb5aa9cc10ac
michalwilk <michal.wilk0@yahoo.com>
2022-10-24 17:29:05 +0200

    add function resolver

--- src/wolth/server/resolvers.clj
+++ src/wolth/server/resolvers.clj
@@ -1,8 +1,12 @@
 (ns wolth.server.resolvers
-  (:require [wolth.server.exceptions :refer
-             [throw-wolth-exception def-interceptor-fn]]
+  (:require [wolth.server.exceptions :refer [def-interceptor-fn]]
             [wolth.server.config :refer [def-context cursor-pool]]
-            [wolth.server.-test-data :refer [_resolver_serializer_test_data]]
+            [wolth.utils.common :refer [multiple-get]]
+            [wolth.utils.loader :refer
+             [load-app-functions! create-namespace-name]]
+            [wolth.server.-test-data :refer
+             [_resolver_model_ctx_test_data _resolver_func_ctx_test_data
+              _resolver_test_function_data _resolver_func_test_namespace]]
             [io.pedestal.log :as log]
             [wolth.db.utils :refer [execute-sql-expr!]]
             [next.jdbc :as jdbc]))
@@ -12,24 +16,48 @@
 (def-context _test-context
              {cursor-pool {"app" (jdbc/get-datasource {:dbtype "h2",
                                                        :dbname "testing"})}})
-(def-interceptor-fn
-  resolve-model-query
-  [ctx]
-  (log/info ::resolve-model-query (select-keys ctx [:sql-query]))
-  (if-not (contains? ctx :sql-query)
-    (throw-wolth-exception :500 "No SQL query found in context!")
-    (let [query (ctx :sql-query)
-          result (execute-sql-expr! (get ctx :app-name) query)]
-      (as-> ctx it (dissoc it :sql-query) (assoc it :result result)))))
+
+
+(defn- resolve-model
+  [ctx sql-query]
+  (let [result (execute-sql-expr! (get ctx :app-name) sql-query)]
+    (as-> ctx it (dissoc it :sql-query) (assoc it :model-result result))))
+
+(defn- resolve-bank
+  [ctx func-data]
+  (log/info ::resolve-bank (str "Resolving function: " func-data))
+  (let [app-name (ctx :app-name)
+        [func-name f-args] (multiple-get func-data
+                                         (list :function-name :function-args))
+        function-path (->> func-name
+                           (str (create-namespace-name app-name) "/")
+                           (symbol))]
+    (assert list? f-args)
+    (assoc ctx :bank-result (apply (eval function-path) f-args))))
 
 (comment
-  (_test-context '(resolve-model-query _resolver_serializer_test_data)))
+  (load-app-functions! _resolver_func_test_namespace
+                       _resolver_test_function_data)
+  (resolve-bank {:app-name "test-app"}
+                {:function-name "getDate", :function-args (list 997)})
+  (resolve-bank {:app-name "person"}
+                {:function-name "getPrimes", :function-args (list 997)}))
 
-(def model-resolver-interceptor
-  {:name ::MODEL-RESOLVER-INTERCEPTOR, :enter resolve-model-query})
+(def-interceptor-fn resolve-query
+                    [ctx]
+                    (log/info ::resolve-model-query
+                              (select-keys ctx [:sql-query :function-data]))
+                    (let [sql-query (get ctx :sql-query)
+                          func-data (get ctx :function-data)]
+                      (cond (some? sql-query) (resolve-model ctx sql-query)
+                            (some? func-data) (resolve-bank ctx func-data))))
+
+(comment
+  (_test-context '(resolve-query _resolver_model_ctx_test_data))
+  (_test-context '(resolve-query _resolver_func_ctx_test_data)))
+
+(def wolth-resolver-interceptor
+  {:name ::RESOLVER-INTERCEPTOR, :enter resolve-query})
 
 (defn create-database-driver [])
-
-(def function-resolver-interceptor
-  {:name ::MODEL-RESOLVER-INTERCEPTOR, :enter resolve-model-query})
 
