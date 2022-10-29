e72ea5b32cccfd7793e6240d90f9876e1f24bfa3
michalwilk <michal.wilk0@yahoo.com>
2022-10-30 01:08:12 +0200

    harden uriql parser and test for bugs

--- src/wolth/db/urisql_parser.clj
+++ src/wolth/db/urisql_parser.clj
@@ -1,18 +1,17 @@
 (ns wolth.db.urisql-parser
   (:require [clojure.string :as str]
-            [wolth.utils.common :refer [find-first cons-not-nil]]
-            [wolth.server.exceptions :refer [throw-wolth-exception]]))
+            [wolth.utils.common :refer
+             [find-first cons-not-nil seqs-equal? trim-string]]))
 
 (defn throw-uriql-exception
   [info]
-  (throw (ex-info "UriqlException" {:info (str "Uriql Error: " info)})))
+  (throw (RuntimeException. (str "UriqlException. " info))))
 
 (defmacro def-named-token
   [name & exprs]
   `(def ~name (re-pattern (format "(?<%s>%s)" '~name (str ~@exprs)))))
 
 (defn alt-regs [& regs] (re-pattern (str/join "|" regs)))
-(defn multi-regs [reg] (re-pattern (str "(" reg ")+")))
 
 (def-named-token startBracket "\\(")
 (def-named-token endBracket "\\)")
@@ -50,8 +49,9 @@
 ;;;;;;;;; URIQL base syntax ;;;;;;;;;;
 (def-named-token symbolToken
                  (alt-regs functionToken logicalToken comparisonToken))
-(def-named-token fieldToken "\"[a-zA-Z]([a-zA-Z0-9\\- $]|(\\\\\"))*\"")
-(def-named-token valueToken "'([a-zA-Z0-9\\- $%#&=+*]+)'")
+(def-named-token fieldToken "\"[a-zA-Z]([a-zA-Z0-9\\-$])*\"")
+(def-named-token valueToken
+                 "'([a-zA-Z0-9\\-\\[\\]{}:;.,/?<> !@#$%^&*()-_=+]+)'")
 
 (def symbol-groups ["functionToken" "comparisonToken" "logicalToken"])
 
@@ -69,7 +69,7 @@
   [token-str tok-group]
   (let [matcher-obj (re-matcher tokenizer-query token-str)]
     (when-not (.matches matcher-obj)
-      (throw-wolth-exception :500 (str "cannot parse symbol: " token-str)))
+      (throw-uriql-exception (str "Cannot parse symbol: " token-str)))
     (find-first (fn [el] (.group matcher-obj el)) tok-group)))
 
 (comment
@@ -80,7 +80,8 @@
 (defn test-for-syntax-errors
   [base-str parsed-strs]
   (when (not= (str/join "" parsed-strs) base-str)
-    (throw-wolth-exception :400 "Uriql syntax error"))
+    (throw-uriql-exception (str "Detected unknown tokens in uriql string: "
+                                base-str)))
   parsed-strs)
 
 (defn tokenize
@@ -94,11 +95,16 @@
 
 (def _test_tokens
   ["filter(\"name\"=='1'and\"surname\"=='Kowalski')sorta(\"name\")sortd(\"surname\")"
-   "filter(\"name\"=='John')"])
+   "filter(\"name\"=='John')" "'Lorem Ipsum (123) 111-222-333'"
+   "filter(\"name\"=='123 __===++--*^&*%$#@!')"
+   "'123 __===++--*^&*%$#@![]{},.?<>')"])
 
 (comment
   (tokenize (_test_tokens 0))
-  (tokenize (_test_tokens 1)))
+  (tokenize (_test_tokens 1))
+  (tokenize (_test_tokens 2))
+  (tokenize (_test_tokens 3))
+  (tokenize (_test_tokens 4)))
 
 (def uriql-parse-struct
   {:symbols '(),
@@ -109,18 +115,27 @@
    :limit-expr nil,
    :bracket-ctr 0})
 
-(defn- trim-string [text] (subs text 1 (dec (.length text))))
-
 (def comp-transform-map {"==" :=, "<>" :<>, ">" :>, "<" :<, "<=" :<=, ">=" :>=})
 
 (def logical-transform-map {"and" :and, "or" :or})
 
-(def sort-transform-map {"and" :and, "or" :or})
-
 (defn- build-single-filter-operation
   [val-map]
+  (when (not= (count val-map) 3)
+    (throw-uriql-exception
+      (format
+        "Have not provided all nessessary fields to build a comparison expression. Missing fields: %s. Tried to evaluate operation: %s"
+        (vec (remove (partial get val-map)
+               ["comparisonToken" "fieldToken" "valueToken"]))
+        val-map)))
   [(comp-transform-map (val-map "comparisonToken"))
-   (trim-string (val-map "fieldToken")) (trim-string (val-map "valueToken"))])
+   (keyword (trim-string (val-map "fieldToken")))
+   (trim-string (val-map "valueToken"))])
+
+(comment
+  (build-single-filter-operation
+    {"comparisonToken" "==", "fieldToken" "\"aa\"", "valueToken" "'bb"})
+  (build-single-filter-operation {"fieldToken" "\"aa\"", "valueToken" "'bb"}))
 
 (defn- reduce-filter-expr
   [vals]
@@ -137,13 +152,13 @@
 (defn- eval-logic-filter-expr
   [l-val operation-str r-val]
   (let [operation (or (logical-transform-map operation-str)
-                      (throw (RuntimeException. (str "Unknown operation: "
-                                                     operation-str))))]
+                      (throw-uriql-exception (str "Unknown operation: "
+                                                  operation-str)))]
     (if (= operation (first r-val) (first l-val))
       [operation (vec (rest r-val)) (vec (rest l-val))]
       [operation r-val l-val])))
 
-(defn evaluate-filter-stack
+(defn- evaluate-filter-stack
   [acc]
   (let [updated-acc (-> acc
                         (update :bracket-ctr dec)
@@ -161,7 +176,8 @@
 
 (defn- filter-reducer-fn
   [acc item]
-  (when (neg? (acc :bracket-ctr)) (assert false))
+  (when (neg? (acc :bracket-ctr))
+    (throw-uriql-exception "Detected not closed bracket"))
   (case (item :type)
     "endBracket" (update acc :bracket-ctr inc)
     "startBracket" (evaluate-filter-stack acc)
@@ -179,15 +195,15 @@
   (let [reduced-exprs
           (reduce filter-reducer-fn {:bracket-ctr 0, :stack nil} stack)]
     (when (not (zero? (reduced-exprs :bracket-ctr)))
-      (throw (RuntimeException.
-               "Uriql Error. Unmatched bracket in filter statement")))
+      (throw-uriql-exception "Unmatched bracket in filter statement"))
     (when (not= (count (reduced-exprs :stack)) 1)
-      (throw (RuntimeException.
-               (str "Uriql Error. Not all statements were evaluated: "
-                    (reduced-exprs :stack)))))
+      (throw-uriql-exception
+        (format
+          "Not all filter statements could be evaluated: %s. Try to use brackets to separate filter expressions."
+          (vec (rest (reduced-exprs :stack))))))
     (when (some? (reduced-exprs :logical-operation))
-      (throw (RuntimeException. (str "Uriql Error. Redundant logical operand: "
-                                     (reduced-exprs :logical-operation)))))
+      (throw-uriql-exception (str "Redundant logical operand: "
+                                  (reduced-exprs :logical-operation))))
     (-> reduced-exprs
         (:stack)
         (first))))
@@ -206,35 +222,23 @@
                        {:type "valueToken", :value "'Alice'"}
                        {:type "comparisonToken", :value ">"}
                        {:type "fieldToken", :value "\"secName\""}
-                       {:type "startBracket", :value "("}))
-  (reduce-filter (list {:type "endBracket", :value ")"}
-                       {:type "endBracket", :value ")"}
-                       {:type "valueToken", :value "'John'"}
-                       {:type "comparisonToken", :value "=="}
-                       {:type "fieldToken", :value "\"name\""}
-                       {:type "logicalToken", :value "and"}
-                       {:type "valueToken", :value "'Alice'"}
-                       {:type "comparisonToken", :value ">"}
-                       {:type "fieldToken", :value "\"secName\""}
-                       {:type "startBracket", :value "("}
-                       {:type "logicalToken", :value "and"}
-                       {:type "endBracket", :value ")"}
-                       {:type "valueToken", :value "'Alice1'"}
-                       {:type "comparisonToken", :value ">"}
-                       {:type "fieldToken", :value "\"secName1\""}
-                       {:type "logicalToken", :value "or"}
-                       {:type "valueToken", :value "'John1'"}
-                       {:type "comparisonToken", :value "=="}
-                       {:type "fieldToken", :value "\"name1\""}
-                       {:type "startBracket", :value "("}
                        {:type "startBracket", :value "("})))
+(def sort-stack-types (list "endBracket" "fieldToken" "startBracket"))
 
 (defn reduce-sort
   [stack direction]
-  (println stack)
+  (when (not= (count stack) 3)
+    (throw-uriql-exception
+      (format
+        "Incorrect number of arguments to a sort function. Got %s, expected 1"
+        (- (count stack) 2))))
+  (when-not (seqs-equal? (map :type stack) sort-stack-types)
+    (throw-uriql-exception
+      "Provided wrong type of arguments to a sort function. Expecting one fieldToken (the one surrounded with double brackets)"))
   [(-> stack
        (second)
        (:value)
+       (trim-string)
        (keyword)) direction])
 
 (defn reduce-uriql-stack
@@ -261,8 +265,7 @@
       "sortd" (update trimmed-struct
                       :sort-exprs
                       #(conj % (reduce-sort sym-stack :desc)))
-      (throw (RuntimeException. (format "Urisql error: Unknown symbol %s"
-                                        fn-symbol))))))
+      (throw-uriql-exception "Urisql error: Provided unknown function name"))))
 
 (comment
   (reduce-uriql-stack {:symbols (list {:type "endBracket", :value ")"}
@@ -288,25 +291,33 @@
 
 (defn create-parse-stucture
   [tokens]
-  (letfn [(append-token [struct tok]
-            (if (= (tok :type) "functionToken")
-              (assoc struct :fn-symbol (tok :value))
-              (update struct :symbols (partial cons tok))))
-          (update-bracket-count [struct tok]
-            (case (tok :type)
-              "endBracket" (update struct :bracket-ctr dec)
-              "startBracket" (update struct :bracket-ctr inc)
-              struct))
-          (reduce-stack-if-needed [struct tok]
-            (if (and (zero? (struct :bracket-ctr)) (= (tok :type) "endBracket"))
-              (reduce-uriql-stack struct)
-              struct))
-          (reducer-fn [struct item]
-            (-> struct
-                (append-token item)
-                (update-bracket-count item)
-                (reduce-stack-if-needed item)))]
-    (reduce reducer-fn uriql-parse-struct tokens)))
+  (letfn
+    [(append-token [struct tok]
+       (if (= (tok :type) "functionToken")
+         (assoc struct :fn-symbol (tok :value))
+         (update struct :symbols (partial cons tok))))
+     (update-bracket-count [struct tok]
+       (case (tok :type)
+         "endBracket" (update struct :bracket-ctr dec)
+         "startBracket" (update struct :bracket-ctr inc)
+         struct))
+     (reduce-stack-if-needed [struct tok]
+       (if (and (zero? (struct :bracket-ctr)) (= (tok :type) "endBracket"))
+         (reduce-uriql-stack struct)
+         struct))
+     (reducer-fn [struct item]
+       (-> struct
+           (append-token item)
+           (update-bracket-count item)
+           (reduce-stack-if-needed item)))
+     (parsed-correctly? [parsed-struct]
+       (when (not-empty (parsed-struct :symbols))
+         (throw-uriql-exception (str "Some symbols could not be evaluated: "
+                                     (parsed-struct :symbols))))
+       parsed-struct)]
+    (->> tokens
+         (reduce reducer-fn uriql-parse-struct)
+         (parsed-correctly?))))
 
 (comment
   (create-parse-stucture (list {:type "functionToken", :value "filter"}
@@ -326,39 +337,94 @@
 
 (comment
   (unfold-symbol-tokens (list {:type "symbolToken", :value "filter"}
-                              {:type "startBracket", :value "("}
-                              {:type "fieldToken", :value "\"name\""}
-                              {:type "symbolToken", :value "=="}
-                              {:type "valueToken", :value "'John'"}
-                              {:type "endBracket", :value ")"})))
+                              {:type "symbolToken", :value "=="})))
 
-(defn validate-uriql-commands
+(defn validate-uriql-commands!
   [tokens available-commands]
-  (if (nil? available-commands) tokens (do (run! true? tokens) tokens)))
+  (if (nil? available-commands)
+    tokens
+    (do (run! (fn [it]
+                (when (and (= (it :type) "functionToken")
+                           (nil? (available-commands (it :value))))
+                  (throw-uriql-exception (str "Forbidded function token used: "
+                                              (it :value)))))
+              tokens)
+        tokens)))
+
+(comment
+  (validate-uriql-commands! (list {:type "functionToken", :value "filter"}
+                                  {:type "startBracket", :value "("}
+                                  {:type "fieldToken", :value "\"name\""})
+                            #{"filter"})
+  (validate-uriql-commands! (list {:type "functionToken", :value "filter"}
+                                  {:type "startBracket", :value "("}
+                                  {:type "fieldToken", :value "\"name\""})
+                            #{"sorta"}))
+
+(defn build-honeysql-exprs
+  [parse-struct]
+  (letfn [(merge-where-clauses [filter-exps]
+            (if (= (count filter-exps) 1)
+              (first filter-exps)
+              (apply vector (cons :and filter-exps))))]
+    (cond-> parse-struct
+      (not-empty (parse-struct :filter-exprs))
+        (assoc :where (merge-where-clauses (parse-struct :filter-exprs)))
+      (not-empty (parse-struct :sort-exprs)) (assoc :order-by
+                                               (parse-struct :sort-exprs))
+      (parse-struct :offset-expr) (assoc :offset (parse-struct :offset-expr))
+      (parse-struct :limit-expr) (assoc :limit (parse-struct :limit-expr))
+      :finally (select-keys [:where :order-by :offset :limit]))))
+
+(defn apply-uriql-syntax-sugar
+  [query]
+  (cond (re-matches allToken query) ""
+        (re-matches compOnlyToken query) (str "filter(" query ")")
+        :else query))
+
+(comment
+  (apply-uriql-syntax-sugar "filter(\"name\"=='1')")
+  (apply-uriql-syntax-sugar "*")
+  (apply-uriql-syntax-sugar "\"id\"=='1234'"))
 
 (defn parse-uriql
   [query & [available-commands]]
   (-> query
       (tokenize)
-      (validate-uriql-commands available-commands)
       (unfold-symbol-tokens)
-      (create-parse-stucture)))
+      (validate-uriql-commands! available-commands)
+      (create-parse-stucture)
+      (build-honeysql-exprs)))
 
+;; Correct queries
 (comment
-  (parse-uriql "filter(\"name\"=='1')")
+  ;; (parse-uriql "\"id\"=='jo8wqn-2189nd-a7sas2-qeqwb1'")
   (parse-uriql
-    "filter(\"name\"=='1'and\"surname\"<>'Kowalski')sortd(\"name\")"))
+    "filter(\"name\"=='1'and\"age\">'10')filter(\"surname\"<>'Kowalski')")
+  (parse-uriql "filter(\"name\"=='Lorem Ipsum\\( 123 111-222-333')")
+  (parse-uriql "filter(\"name\"=='123 __===++--*^&*%$#@!')")
+  (parse-uriql "sorta(\"name\")")
+  (parse-uriql "sorta(\"name\")" #{"filter" "sorta"})
+  (parse-uriql "sorta(\"name\")sortd(\"created-at\")")
+  (parse-uriql
+    "filter(\"name\"=='1'and\"surname\"<>'Kowalski')sortd(\"name\")filter(\"age\">'10')")
+  (parse-uriql "")
+  (parse-uriql "filter((\"aa\"=='bb'and\"cc\">'10')or\"dd\"<>'ee')")
+  (parse-uriql
+    "sortd(\"surname\")filter((\"aa\"=='bb'and\"cc\">'10')or\"dd\"<>'ee')sorta(\"name\")")
+  (parse-uriql
+    "filter((\"aa\"=='()bb'and(\"cc\">'10'and(\"ff\">'1'or\"gg\"<'10')))or\"dd\"<>'ee')"))
 
-(defn filter-query->uriql [query] (str "filter(" query ")"))
-
-(defn apply-proper-uriql-syntax
-  [query]
-  (cond (re-matches allToken query) (parse-uriql "")
-        (re-matches compOnlyToken query) (parse-uriql (filter-query->uriql
-                                                        query))
-        :else (parse-uriql query)))
-
+;; Incorrect queries
 (comment
-  (apply-proper-uriql-syntax "filter(\"name\"=='1')")
-  (apply-proper-uriql-syntax "*")
-  (apply-proper-uriql-syntax "\"id\"=='1234'"))
+  (parse-uriql "lalalalal")
+  (parse-uriql "sorta(\"name\")" #{"filter"})
+  (parse-uriql "sorta('1')")
+  (parse-uriql "lalal()")
+  (parse-uriql "sorta(\"name\" \"lalala\")")
+  (parse-uriql "sorta(sorta(\"lll\"))")
+  (parse-uriql "filter((\"aa\"=='bb'and\"cc\">10)or\"dd\"<>'ee')")
+  (parse-uriql
+    "filter(\"name\">='10'and\"surname\"<>'Kowalski'and\"aa\"=='bb')")
+  (parse-uriql "filter(\"name\">='10'and\"surname\"<>'Kowalski')("))
+
